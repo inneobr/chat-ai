53775f01ac1add82472d2be17cb578722baf4c8d
eduardo.cruz <tecnologiaesolucoespr@gmail.com>
2023-07-30 10:53:52 -0300

    feat

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: "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"
 })
 
 const openai = new OpenAIApi(config);
