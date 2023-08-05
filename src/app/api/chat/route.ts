4cf0f87e9c7008fdfbe4e8b1ffb2f2af1d7a599f
eduardo.cruz <tecnologiaesolucoespr@gmail.com>
2023-08-04 23:53:03 -0300

    feature: route

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -13,17 +13,12 @@ export const runtime = "edge";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
-  try {
-    const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      stream: true,
-      messages
-    })
-
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages
+  })
     
-    const stream = OpenAIStream(response)
-    return new StreamingTextResponse(stream); 
-  }catch(e){
-    console.log(e)
-  }
+  const stream = OpenAIStream(response)
+  return new StreamingTextResponse(stream);  
 }
