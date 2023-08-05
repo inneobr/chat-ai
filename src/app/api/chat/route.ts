a72e65246c479753cb44b1762f8f199813c2f571
eduardo.cruz <tecnologiaesolucoespr@gmail.com>
2023-08-05 00:11:45 -0300

    fat

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -5,18 +5,25 @@ const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 })
 
+console.log(process.env.OPENAI_API_KEY)
+
 const openai = new OpenAIApi(config);
 
 export const runtime = "edge";
 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
-  const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages
-  })
+  try{
+    const { messages } = await req.json();
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      stream: true,
+      messages
+    })
 
-  const stream = OpenAIStream(response)
-  return new StreamingTextResponse(stream);  
+
+    const stream = OpenAIStream(response)
+    return new StreamingTextResponse(stream); 
+  }catch(e){
+    console.log(e)
+  } 
 }
